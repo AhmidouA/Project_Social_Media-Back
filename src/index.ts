528d00daabf8786754572ab32c895e68a2fbe24d
AhmidouA <ahmedakkioui75@gmail.com>
2024-01-22 20:52:42 +0100

    dependencies and setUp Done

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
+/* Npm */
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from "url"; //  sert à convertir un objet URL en un chemin d'accès de fichier utilisable dans le système de fichiers en JavaScript
+
+/* CONFIGURATION */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 dotenv.config({path: './src/config/.env'})
 
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
+app.use(express.json({ limit: '30mb'}));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(morgan("common"));
+
+app.use(express.static("../public/"));
+
 
 const PORT = process.env.PORT;
 

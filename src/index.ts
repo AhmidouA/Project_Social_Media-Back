89c4c5235df2bdf872d6e1061acd4ab9a9f8be37
AhmidouA <ahmedakkioui75@gmail.com>
2024-01-23 17:21:30 +0100

    Multer config

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,11 @@ import multer from 'multer';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
-import { fileURLToPath } from "url"; //  sert à convertir un objet URL en un chemin d'accès de fichier utilisable dans le système de fichiers en JavaScript
+import cors from 'cors';
+import { fileURLToPath } from "node:url"; //  sert à convertir un objet URL en un chemin d'accès de fichier utilisable dans le système de fichiers en JavaScript
 
 /* CONFIGURATION */
-const __filename = fileURLToPath(import.meta.url);
+const __filename = path.resolve();
 const __dirname = path.dirname(__filename);
 
 
@@ -21,14 +22,14 @@ app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
 app.use(express.json({ limit: '30mb'}));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(morgan("common"));
+app.use(cors());
 
-app.use(express.static("../public/"));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 
 const PORT = process.env.PORT;
 
-
-
+// app.use('/api',)
 
 app.listen(PORT, () => {
     console.log(`server runnig on ${PORT}`)
